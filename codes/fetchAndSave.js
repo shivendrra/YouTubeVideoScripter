d0029d5dc9b10817a86eb01d78c088c4fa95dfe4
Shivendra <shivharsh44@gmail.com>
2024-01-04 23:19:57 +0530

    some new changes

--- codes/fetchAndSave.js
+++ codes/fetchAndSave.js
@@ -2,38 +2,45 @@ const fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const targetUrl = 'https://www.nasa.gov/';
+const jsonData = fs.readFileSync('../data/search_results.json')
+const searchResults = JSON.parse(jsonData);
 const outputFileName = '../data/output.txt';
 
 async function fetchData(url) {
   try {
     const response = await axios.get(url);
     return response.data;
-  } catch (error) {
-    console.error('Error fetching data:', error.message);
-    process.exit(1);
+  } 
+  catch (error) {
+    console.error(`Error fetching data from ${url}:`, error.message);
+    return null;
   }
 }
 
 function extractTextFromHtml(html) {
   const $ = cheerio.load(html);
   const paragraphs = $('p').map((index, element) => $(element).text()).get();
-  return paragraphs.join('\n');
+  return paragraphs.join();
 }
 
 async function saveTextToFile(text, fileName) {
   try {
     fs.writeFileSync(fileName, text);
     console.log(`Text data saved to ${fileName}`);
-  } catch (error) {
+  }
+  catch (error) {
     console.error('Error saving data to file:', error.message);
   }
 }
 
 async function main() {
-  const htmlData = await fetchData(targetUrl);
-  const textData = extractTextFromHtml(htmlData);
-  await saveTextToFile(textData, outputFileName);
+  for (const result of searchResults) {
+    const htmlData = await fetchData(result.url);
+    if (htmlData) {
+      const textData = extractTextFromHtml(htmlData);
+      await saveTextToFile(textData, outputFileName);
+    }
+  }
 }
 
 main();
