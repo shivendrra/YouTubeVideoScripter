2f340abb4cbc53830ec468e103ac85ffb2cff108
Shivendra <shivharsh44@gmail.com>
2024-01-07 19:55:48 +0530

    new scrapping code for britannica.com

--- codes/customSearch.js
+++ codes/customSearch.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const puppeteer = require('puppeteer');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
@@ -9,7 +10,6 @@ dotenv.config({ path: dotenvPath });
 
 const api_key = process.env.search_key;
 const cx_id = process.env.cx_id;
-
 const file_path = '../data/search_strings.json';
 const search_strings = JSON.parse(fs.readFileSync(file_path, 'utf-8'));
 const outputFileName = '../data/output.json';
@@ -18,25 +18,37 @@ let pageNo = 1
 
 const start_time = Date.now();
 
-async function gSearch(searchTerm, cxId, pageNo) {
-  const formattedSearchTerm = searchTerm.split(' ').join('%');
-  const url = `https://cse.google.com/cse?cx=${cxId}#gsc.tab=0&gsc.q=${formattedSearchTerm}&gsc.sort=&gsc.page=${pageNo}`;
-  const response = await axios.get(url);
-  return response;
+async function scrapeContent(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  // Navigate to the URL
+  await page.goto(url);
+  const content = await page.content();
+  fs.appendFileSync('../faaltu.html', content)
+  await browser.close();
+  return content;
 }
+
 
 async function scrapeLinks(htmlContent) {
   const $ = cheerio.load(htmlContent);
   const links = $('a.gs-title').map((index, element) => $(element).attr('href')).get();
-  const actualLinks = links.map(link => new URLSearchParams(link).get('q'));
-  return actualLinks;
+  // const actualLinks = links.map(link => new URLSearchParams(link).get('q'));
+  return links;
+}
+
+async function gSearch(searchTerm, cxId, pageNo) {
+  const formattedSearchTerm = searchTerm.split(' ').join('%');
+  const url = `https://cse.google.com/cse?cx=${cxId}#gsc.tab=0&gsc.q=${formattedSearchTerm}&gsc.sort=&gsc.page=${pageNo}`;
+  return url;
 }
 
 async function main(fileName, pgNo) {
   for (const searchTerm of search_strings) {
-    const response = await gSearch(searchTerm, cx_id, pgNo);
-    if (response.status === 200 && pgNo < 11) {
-      const links = await scrapeLinks(response.data);
+    const url = await gSearch(searchTerm, cx_id, pgNo);
+    if (pgNo < 11) {
+      const scrape = await scrapeContent(url);
+      const links = await scrapeLinks(scrape);
       n_results += 10;
       pgNo += 1;
       fs.appendFileSync(fileName, JSON.stringify({ searchTerm, links }) + '\n');
