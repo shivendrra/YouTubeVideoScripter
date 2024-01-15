e768732537c2cfb68ba88916211298fb026f2074
Shivendra <shivharsh44@gmail.com>
2024-01-16 01:05:35 +0530

    new changes to the text-editor

--- website/react-app/src/components/Lander.js
+++ website/react-app/src/components/Lander.js
@@ -11,45 +11,85 @@ export default function Lander(props) {
   const [textAlign, setTextAlign] = useState('left');
   const [textTransform, setTextTransform] = useState('capitalize');
   const [textColor, setTextColor] = useState('#000000');
-  const [font, setFont] = useState("'Poppins', sans-serif");
+  const [font, setFont] = useState("Poppins");
 
   const handleFontSizeChange = (e) => {
-    let value = e.target.value;
-    setFontSize(value);
+    const inputElement = e.currentTarget.parentNode.querySelector('input[type=number]');
+    const action = e.currentTarget.dataset.action;
+
+    if (action === 'increase') {
+      inputElement.stepUp();
+    } else if (action === 'decrease') {
+      inputElement.stepDown();
+    }
+
+    setFontSize(inputElement.value);
+
+    applyStyleToSelection('fontSize', inputElement.value + 'px');
   }
 
   const handleChangeFont = (e) => {
-    let value = e.target.value;
-    console.log(value);
+    let value = e.target.textContent;
     setFont(value);
-  }
+
+    if (textareaRef.current) {
+      applyStyleToSelection('fontFamily', value);
+    }
+  };
+
+  const applyStyleToSelection = (style, value) => {
+    const textarea = textareaRef.current;
+
+    if (textarea.selectionStart !== undefined && textarea.selectionEnd !== undefined) {
+      const start = textarea.selectionStart;
+      const end = textarea.selectionEnd;
+
+      const selectedText = textarea.value.substring(start, end);
+      const prefix = textarea.value.substring(0, start);
+      const suffix = textarea.value.substring(end);
+
+      const styledText = `<span style="${style}:${value}">${selectedText}</span>`;
+
+      textarea.value = prefix + styledText + suffix;
+    } else {
+      textarea.value += `<span style={${style}:'${value}'}></span>`;
+    }
+  };
+
 
   const handleBoldClick = () => {
     setFontWeight(prevWeight => (prevWeight === 'bold' ? 'normal' : 'bold'));
+    applyStyleToSelection('fontWeight', fontWeight === 'bold' ? 'normal' : 'bold');
   }
 
   const handleItalicClick = () => {
     setFontStyle(prevStyle => (prevStyle === 'italic' ? 'normal' : 'italic'));
+    applyStyleToSelection('fontStyle', fontStyle === 'italic' ? 'normal' : 'italic');
   }
 
   const handleUnderlineClick = () => {
     setTextDecoration(prevDecoration => (prevDecoration === 'underline' ? 'none' : 'underline'));
+    applyStyleToSelection('textDecoration', textDecoration === 'underline' ? 'none' : 'underline');
   }
 
   const handleAlignLeftClick = () => {
     setTextAlign('left');
+    applyStyleToSelection('textAlign', 'left');
   }
 
   const handleAlignCenterClick = () => {
     setTextAlign('center');
+    applyStyleToSelection('textAlign', 'center');
   }
 
   const handleAlignRightClick = () => {
     setTextAlign('right');
+    applyStyleToSelection('textAlign', 'right');
   }
 
   const handleTextTransformClick = () => {
     setTextTransform(prevTransform => (prevTransform === 'uppercase' ? 'none' : 'uppercase'));
+    applyStyleToSelection('textTransform', textTransform === 'uppercase' ? 'none' : 'uppercase');
   }
 
   const handleResetClick = () => {
@@ -60,6 +100,7 @@ export default function Lander(props) {
     setTextAlign('left');
     setTextTransform('capitalize');
     setTextColor('#000000');
+
     if (textareaRef.current) {
       textareaRef.current.value = '';
     }
@@ -67,6 +108,7 @@ export default function Lander(props) {
 
   const handleColorChange = (e) => {
     setTextColor(e.target.value);
+    applyStyleToSelection('color', e.target.value);
   }
 
   useEffect(() => {
@@ -85,18 +127,20 @@ export default function Lander(props) {
           <div className="col-lg-9 text-col">
             <div className="col-lg-12 text-options">
               <div className="option-sec font-change">
-                <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" onChange={handleChangeFont}>{props.font}</button>
+                <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{font}</button>
                 <ul class="dropdown-menu">
-                  <li className='dropdown-item'>Courier Prime</li>
-                  <li className='dropdown-item'>Josefine Sans</li>
-                  <li className='dropdown-item'>Poppins</li>
-                  <li className='dropdown-item'>Roboto</li>
-                  <li className='dropdown-item'>Space Grotesk</li>
-                  <li className='dropdown-item'>Space Mono</li>
+                  <li className='dropdown-item' onClick={handleChangeFont}>Courier Prime</li>
+                  <li className='dropdown-item' onClick={handleChangeFont}>Josefine Sans</li>
+                  <li className='dropdown-item' onClick={handleChangeFont}>Poppins</li>
+                  <li className='dropdown-item' onClick={handleChangeFont}>Roboto</li>
+                  <li className='dropdown-item' onClick={handleChangeFont}>Space Grotesk</li>
+                  <li className='dropdown-item' onClick={handleChangeFont}>Space Mono</li>
                 </ul>
               </div>
               <div className="option-sec first">
-                <input type="number" value={fontSize} min='1' max='100' onChange={handleFontSizeChange} />
+                <button onClick={handleFontSizeChange} data-action="decrease">-</button>
+                <input type="number" className="name" min="1" max='100' value={fontSize} onChange={handleFontSizeChange} />
+                <button onClick={handleFontSizeChange} data-action="increase" className="plus">+</button>
               </div>
               <div className="second option-sec">
                 <button type="button" onClick={handleBoldClick}>
@@ -140,7 +184,7 @@ export default function Lander(props) {
                   textAlign,
                   textTransform,
                   color: textColor,
-                  fontFamily: `${font}`
+                  fontFamily: font
                 }}
                 aria-label="With textarea"
                 placeholder='Write your Script here'
